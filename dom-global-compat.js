756c79d99bc1f8cf587ef147eefcb5525e500977
domenicleonetti8-dev <domenicleonetti8@gmail.com>
2026-09-10 18:27:58 -0400

    Expose canonical ingress and broker modules through compatibility bridge

--- dom-global-compat.js
+++ dom-global-compat.js
@@ -8,9 +8,12 @@
   try{if(typeof DOMPlanetaryEcosystemDefense!=='undefined')exports.DOMPlanetaryEcosystemDefense=DOMPlanetaryEcosystemDefense}catch(_){}
   try{if(typeof DOMPublicSensorRegistry!=='undefined')exports.DOMPublicSensorRegistry=DOMPublicSensorRegistry}catch(_){}
   try{if(typeof DOMSensorActivation!=='undefined')exports.DOMSensorActivation=DOMSensorActivation}catch(_){}
+  try{if(typeof DOMObservationIngress!=='undefined')exports.DOMObservationIngress=DOMObservationIngress}catch(_){}
+  try{if(typeof DOMObservationBroker!=='undefined')exports.DOMObservationBroker=DOMObservationBroker}catch(_){}
   try{if(typeof DOMGlobalSensorGlobe!=='undefined')exports.DOMGlobalSensorGlobe=DOMGlobalSensorGlobe}catch(_){}
   try{if(typeof DOMSHazardResearch!=='undefined')exports.DOMSHazardResearch=DOMSHazardResearch}catch(_){}
   try{if(typeof DOMSHazardVoice!=='undefined')exports.DOMSHazardVoice=DOMSHazardVoice}catch(_){}
   for(const [k,v] of Object.entries(exports)){if(!window[k])window[k]=v}
-  window.DOMGlobalCompat={count:Object.keys(exports).length,exports:Object.keys(exports),ok:Object.keys(exports).length>=11};
+  const required=['DOMObservationModel','DOMHazardTaxonomy','DOMEnvironmentalFusion','DOMEnvironmentalDefense','DOMPlanetHealth','DOMPlanetaryEcosystemDefense','DOMPublicSensorRegistry','DOMSensorActivation','DOMObservationIngress','DOMObservationBroker','DOMGlobalSensorGlobe','DOMSHazardResearch','DOMSHazardVoice'];
+  window.DOMGlobalCompat={count:Object.keys(exports).length,exports:Object.keys(exports),missing:required.filter(k=>!exports[k]),ok:required.every(k=>!!exports[k])};
 })();
