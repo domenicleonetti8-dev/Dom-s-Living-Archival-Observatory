5ac643e54effae6e8d953d65ac191bac159a0e86
domenicleonetti8-dev <domenicleonetti8@gmail.com>
2026-09-10 22:37:11 -0400

    Add native DOMS Apple AR app entrypoint

---
+++ apple-ar/DOMSAppleAR/DOMSAppleARApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct DOMSAppleARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
