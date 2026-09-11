c21b80c68c95223194785a35a6c947f95b377c7b
domenicleonetti8-dev <domenicleonetti8@gmail.com>
2026-09-11 16:45:05 -0400

    Register NHC broker adapter in regression expectations

--- broker/test_dom_observation_broker.py
+++ broker/test_dom_observation_broker.py
@@ -48,7 +48,7 @@ class BrokerTests(unittest.TestCase):
         try:
             self.assertEqual(len(b.sources), len(dom.REGISTERED_SOURCE_IDS))
             self.assertEqual(set(b.adapters), {
-                "usgs-eq", "nasa-eonet", "nws-alerts", "swpc", "ntwc", "ptwc",
+                "usgs-eq", "nasa-eonet", "nws-alerts", "swpc", "ntwc", "ptwc", "nhc-tropical",
                 "earthscope-fdsn", "usgs-water-sites",
             })
             self.assertEqual(b.sources["wmo-gos"].status, "registered-not-ingesting")
