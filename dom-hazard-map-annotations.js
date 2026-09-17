26ca031edbccb7b15f99c67776147fc5b78aafbb
domenicleonetti8-dev <domenicleonetti8@gmail.com>
2026-09-17 06:53:32 -0400

    Add FIRMS thermal anomalies to preserved hazard modules

--- dom-hazard-map-annotations.js
+++ dom-hazard-map-annotations.js
@@ -1,6 +1,7 @@
 (()=>{'use strict';
 Promise.allSettled([
   import('./dom-hazard-visibility-repair.js?v=20260917-1'),
-  import('./dom-global-realtime-hazard-bridge.js?v=20260917-1')
+  import('./dom-global-realtime-hazard-bridge.js?v=20260917-1'),
+  import('./dom-firms-thermal-anomaly-bridge.js?v=20260917-1')
 ]).then(r=>{for(const x of r)if(x.status==='rejected')console.warn('D.O.M. additive hazard module load failed',x.reason)});
 })();
